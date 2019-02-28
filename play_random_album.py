d4ad648b9983cf66ac8d3e02bfacab1d145f1a0d
Kevin Johnson <geonson@gmail.com>
2019-02-28 11:56:33 -0800

    persist library data in file

--- play_random_album.py
+++ play_random_album.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import subprocess
 import re
 import argparse
+import pickle
 
 class Album:
 	def __init__(self, name):
@@ -35,27 +36,48 @@ class Track:
 		self.actual_track_number = 0
 		self.suggested_track_number = None
 
+	def __getstate__(self):
+		state = {}
+		state['file_object'] = self.file_object
+		state['actual_track_number'] = self.actual_track_number
+		state['suggested_track_number'] = self.suggested_track_number
+		return state
+
+	def __setstate__(self, state):
+		self.file_object = state['file_object']
+		self.muta_file = None
+		self.actual_track_number = state['actual_track_number']
+		self.suggested_track_number = state['suggested_track_number']
+
+	def get_muta_file(self):
+		if self.muta_file is None:
+			self.muta_file = mutagen.File(self.file_object.absolute())
+		return self.muta_file
+
 	def get_album_names(self):
 		album_names = []
-		if u'Album' in self.muta_file:
-			for album_name in self.muta_file[u'Album']:
+		if u'Album' in self.get_muta_file():
+			for album_name in self.get_muta_file()[u'Album']:
 				album_names.append(album_name)
-		elif u'TALB' in self.muta_file:
-			for album_name in self.muta_file[u'TALB']:
+		elif u'TALB' in self.get_muta_file():
+			for album_name in self.get_muta_file()[u'TALB']:
 				album_names.append(album_name)
 		return album_names
 
 	def find_actual_track_number(self):
-		if u'TRCK' in self.muta_file:
-			for track_number in self.muta_file[u'TRCK']:
+		if self.actual_track_number is not 0:
+			#already has actual track number
+			pass
+		if u'TRCK' in self.get_muta_file():
+			for track_number in self.get_muta_file()[u'TRCK']:
 				actual_number = int(track_number.split('/')[0])
 				self.actual_track_number = actual_number
-		elif u'TPOS' in self.muta_file:
-			for track_number in self.muta_file[u'TPOS']:
+		elif u'TPOS' in self.get_muta_file():
+			for track_number in self.get_muta_file()[u'TPOS']:
 				actual_number = int(track_number.split('/')[0])
 				self.actual_track_number = actual_number
-		elif u'TRACKNUMBER' in self.muta_file:
-			for track_number in self.muta_file[u'TRACKNUMBER']:
+		elif u'TRACKNUMBER' in self.get_muta_file():
+			for track_number in self.get_muta_file()[u'TRACKNUMBER']:
 				actual_number = int(track_number.split('/')[0])
 				self.actual_track_number = actual_number
 		else:
@@ -69,12 +91,13 @@ class Track:
 
 	def save_suggested_track_number(self):
 		if self.suggested_track_number is not None:
-			if isinstance(self.muta_file, mutagen.flac.FLAC):
-				self.muta_file['TRACKNUMBER'] = self.suggested_track_number
-			if isinstance(self.muta_file, mutagen.id3.ID3FileType):
-				self.muta_file.tags['TRCK'] = mutagen.id3.TRCK(encoding=mutagen.id3.Encoding.LATIN1, text=u'{0}'.format(self.suggested_track_number))
-				#print(self.muta_file.tags)
-			self.muta_file.save()
+			if isinstance(self.get_muta_file(), mutagen.flac.FLAC):
+				self.get_muta_file()['TRACKNUMBER'] = self.suggested_track_number
+			if isinstance(self.get_muta_file(), mutagen.id3.ID3FileType):
+				self.get_muta_file().tags['TRCK'] = mutagen.id3.TRCK(encoding=mutagen.id3.Encoding.LATIN1, text=u'{0}'.format(self.suggested_track_number))
+				#print(self.get_muta_file().tags)
+			self.get_muta_file().save()
+			self.suggested_track_number = None
 
 def play_album_in_vlc(album):
 	album.find_track_numbers()
@@ -90,6 +113,7 @@ def play_album_in_vlc(album):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--all', help='play all albums in a random order', action='store_true')
+parser.add_argument('-r', '--reimport', help='reimport library info', action='store_true')
 args = parser.parse_args()
 if args.all:
 	print('Playing All Albums')
@@ -97,21 +121,28 @@ if args.all:
 all_tracks = []
 all_albums = {}
 
-root_dir = Path('.')
-file_list = [f for f in root_dir.glob('**/*') if f.is_file()]
-
-for file in file_list:
-	pathstring = file.absolute()
-	muta_file = mutagen.File(pathstring)
-	if muta_file is not None:
-		track = Track(file, muta_file)
-		#print(muta_file.info)
-		all_tracks.append(track)
-		album_names = track.get_album_names()
-		for album_name in album_names:
-			if album_name not in all_albums:
-				all_albums[album_name] = Album(album_name)
-			all_albums[album_name].add_track(track)
+if args.reimport or not os.path.isfile('library'):
+	root_dir = Path('.')
+	file_list = [f for f in root_dir.glob('**/*') if f.is_file()]
+	for file in file_list:
+		pathstring = file.absolute()
+		muta_file = mutagen.File(pathstring)
+		if muta_file is not None:
+			track = Track(file, muta_file)
+			#print(muta_file.info)
+			all_tracks.append(track)
+			album_names = track.get_album_names()
+			for album_name in album_names:
+				if album_name not in all_albums:
+					all_albums[album_name] = Album(album_name)
+				all_albums[album_name].add_track(track)
+	library_file = open('library', 'wb')
+	pickle.dump(all_albums, library_file)
+	library_file.close()
+else:
+	library_file = open('library', 'rb')
+	all_albums = pickle.load(library_file)
+	library_file.close()
 
 #print(all_albums.keys())
 

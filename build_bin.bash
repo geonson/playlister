abe36d69cce399ca92a476978e8e7b3eff6b3a91
Kevin Johnson <geonson@gmail.com>
2019-03-15 12:11:16 -0700

    Linux compile

--- build_bin.bash
+++ build_bin.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cython --embed -3 playlister.pyx
-#Mac OS compile
-gcc -Os -I /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/include/python3.6m -L /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/lib -o playlister playlister.c -lpython3.6 -lpthread -lm -lutil -ldl
+if [[ "$OSTYPE" == "linux-gnu" ]]
+then
+  #Linux compile
+  gcc -Os -I /usr/include/python3.6m -L /usr/lib -o playlister playlister.c -lpython3.6m -lpthread -lm -lutil -ldl
+elif [[ "$OSTYPE" == "darwin" ]]
+then
+  #Mac OS compile
+  gcc -Os -I /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/include/python3.6m -L /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/lib -o playlister playlister.c -lpython3.6 -lpthread -lm -lutil -ldl
+fi

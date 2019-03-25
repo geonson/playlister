b4c11b4a5aea677eae54811878899e48e58933e0
Kevin Johnson <geonson@gmail.com>
2019-03-25 10:37:41 -0700

    fixed osx detection, moved binary to bin folder

--- build_bin.bash
+++ build_bin.bash
@@ -4,9 +4,9 @@ cython --embed -3 playlister.pyx
 if [[ "$OSTYPE" == "linux-gnu" ]]
 then
   #Linux compile
-  gcc -Os -I /usr/include/python3.6m -L /usr/lib -o playlister playlister.c -lpython3.6m -lpthread -lm -lutil -ldl
-elif [[ "$OSTYPE" == "darwin" ]]
+  gcc -Os -I /usr/include/python3.6m -L /usr/lib -o bin/playlister playlister.c -lpython3.6m -lpthread -lm -lutil -ldl
+elif [[ "$OSTYPE" == "darwin"* ]]
 then
   #Mac OS compile
-  gcc -Os -I /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/include/python3.6m -L /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/lib -o playlister playlister.c -lpython3.6 -lpthread -lm -lutil -ldl
+  gcc -Os -I /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/include/python3.6m -L /usr/local/Cellar/python/3.6.5/Frameworks/Python.framework/Versions/3.6/lib -o bin/playlister playlister.c -lpython3.6 -lpthread -lm -lutil -ldl
 fi

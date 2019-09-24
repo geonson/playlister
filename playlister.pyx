bd36f729e8da42d6dd9718486f934fc82d60448d
Kevin Johnson <geonson@gmail.com>
2019-09-23 17:18:10 -0700

    sql like parameters

--- playlister.pyx
+++ playlister.pyx
@@ -13,13 +13,13 @@ class Track:
         self.muta_file = muta_file
         self.artist = ''
         self.title = ''
-        self.album_name = ''
-        self.actual_track_number = 0
+        self.album = ''
+        self.track_number = 0
         track_number = None
         if isinstance(self.muta_file, mutagen.flac.FLAC):
             self.artist = self.muta_file.tags['ARTIST'][0]
             self.title = self.muta_file.tags['TITLE'][0]
-            self.album_name = self.muta_file.tags['Album'][0]
+            self.album = self.muta_file.tags['Album'][0]
             track_number = self.muta_file.tags['TRACKNUMBER'][0]
 
         elif isinstance(self.muta_file, mutagen.id3.ID3FileType):
@@ -35,7 +35,7 @@ class Track:
                     self.title = self.muta_file.tags['TIT2'][0]
 
                 if 'TALB' in self.muta_file.tags:
-                    self.album_name = self.muta_file.tags['TALB'][0]
+                    self.album = self.muta_file.tags['TALB'][0]
                     
                 if 'TRCK' in self.muta_file.tags:
                     track_number = self.muta_file.tags['TRCK'][0]
@@ -45,20 +45,20 @@ class Track:
         elif isinstance(self.muta_file, mutagen.asf.ASF):
             self.artist = self.muta_file.tags['Author'][0].value
             self.title = self.muta_file.tags['Title'][0].value
-            self.album_name = self.muta_file.tags['WM/AlbumTitle'][0].value
+            self.album = self.muta_file.tags['WM/AlbumTitle'][0].value
             track_number = self.muta_file.tags['WM/TrackNumber'][0].value
 
         else:
             print('tag parsing not yet supported for type {0}'.format(self.muta_file.__class__.__name__))
 
         if track_number is not None:
-            self.actual_track_number = int(track_number.split('/')[0])
+            self.track_number = int(track_number.split('/')[0])
 
     def __getstate__(self):
         state = {}
         state['file_object'] = self.file_object
-        state['actual_track_number'] = self.actual_track_number
-        state['album_name'] = self.album_name
+        state['track_number'] = self.track_number
+        state['album'] = self.album
         state['artist'] = self.artist
         state['title'] = self.title
         return state
@@ -66,8 +66,8 @@ class Track:
     def __setstate__(self, state):
         self.file_object = state['file_object']
         self.muta_file = None
-        self.actual_track_number = state['actual_track_number']
-        self.album_name = state['album_name']
+        self.track_number = state['track_number']
+        self.album = state['album']
         self.artist = state['artist']
         self.title = state['title']
 
@@ -81,7 +81,12 @@ class Track:
         if isinstance(self.muta_file, mutagen.asf.ASF):
             self.muta_file.tags['WM/TrackNumber'] = mutagen.asf.ASFUnicodeAttribute(u'{0}'.format(track_number))
         self.muta_file.save()
-        self.actual_track_number = track_number
+        self.track_number = track_number
+
+    def __eq__(self, other):
+        if isinstance(other, Track):
+            return str(self.file_object) == str(other.file_object)
+        return False
 
 def validpathtype(arg):
     if os.path.isdir(arg):
@@ -99,7 +104,9 @@ subparsers.required = True
 parser_play = subparsers.add_parser('play', help='play some files')
 parser_play.add_argument('-a', '--all', help='play all items', action='store_true')
 parser_play.add_argument('-t', '--test', help='test mode - print playlist only', action='store_true')
-parser_play.add_argument('order', nargs='+', help='aspect=pattern ...')
+parser_play.add_argument('--where', help='track condition e.g. "artist==\'House Boat\'"')
+parser_play.add_argument('--group_by', help='aspect to create play groups by')
+parser_play.add_argument('order', nargs='+', help='aspect1 [asc|desc|random], aspect2 [asc|desc|random], ...')
 
 parser_import = subparsers.add_parser('import', help='import additional files to library')
 parser_import.add_argument('-r', '--reimport', help='reimport files exsting in library', action='store_true')
@@ -125,60 +132,57 @@ if os.path.isfile(library_filename):
 
 salt = random.randint(0, 999999999)
 
-def sort_track_by_aspects(track):
-    prop_list = []
-    for order in args.order:
-        temp_arr = order.split('=')
-        aspect = temp_arr[0]
-        if aspect == 'album':
-            prop = track.album_name
-        elif aspect == 'artist':
-            prop = track.artist
-        elif aspect == 'track':
-            prop = str(track.actual_track_number)
-        elif aspect == 'title':
-            prop = track.title
-
-        if len(temp_arr) > 1:
-            pattern = temp_arr[1]
-            if pattern == 'random':
-                random.seed(prop+str(salt))
-                prop_list.append(random.randint(0, 999999999))
-            else:
-                reg = re.compile(pattern)
-                if reg.match(prop):
-                    prop_list.append(0)
-                else:
-                    prop_list.append(1)
-        else:
-            prop_list.append(prop)
-    return prop_list
-
 if args.command == 'play':
     play_list = list(tracks_by_filename.values())
+    if args.where:
+        for track in play_list[:]:
+            album = track.album
+            artist = track.artist
+            title = track.title
+            track_number = track.track_number
+            where_result = eval(args.where)
+            if not where_result:
+                play_list.remove(track)
+    #sort by file name by default
     play_list.sort(key=lambda track: str(track.file_object))
-    play_list.sort(key = sort_track_by_aspects)
+    order_string = ' '.join(args.order)
+    comma_separated_orders = order_string.split(',')
+    for order in reversed(comma_separated_orders):
+        trimmed = order.strip()
+        temp_arr = order.split()
+        aspect = temp_arr[0]
+        if aspect:
+            pattern = 'asc'
+            if len(temp_arr) > 1:
+                pattern = temp_arr[1]
+            backwards = False
+            if pattern == 'desc':
+                backwards = True
+            def sort_value_function(track):
+                aspect_value = getattr(track, aspect)
+                if pattern =='random':
+                    random.seed(str(aspect_value)+str(salt))
+                    return random.randint(0, 999999999)
+                else:
+                    return aspect_value
+            play_list.sort(reverse = backwards, key = sort_value_function)
 
     i = 0
     current_first_prop = None
     group_end = 0
+    group_by_aspect = args.order[0]
+    if(args.group_by):
+        group_by_aspect = args.group_by
     while (args.all or group_end == 0) and i <= len(play_list):
         if i < len(play_list):
             track = play_list[i]
-            if args.order[0].startswith('album'):
-                prop = track.album_name
-            elif args.order[0].startswith('artist'):
-                prop = track.artist
-            elif args.order[0].startswith('track'):
-                prop = track.actual_track_number
-            elif args.order[0].startswith('title'):
-                prop = track.title
+            prop = getattr(track, group_by_aspect)
         else:
             prop = ''
         if current_first_prop != prop:
             if i > 0:
                 group_end = i
-                print('\nPlaying {0}\n{1}\n'.format(args.order[0], current_first_prop))
+                print('\nPlaying {0}\n{1}\n'.format(group_by_aspect, current_first_prop))
 
                 vlc_process = None
                 params = []
@@ -187,7 +191,7 @@ if args.command == 'play':
                     if os.path.isfile(str(track.file_object)):
                         params.append(str(track.file_object))
                         if args.test:
-                            print ('{0} - {1} - {2} - {3}'.format(track.artist, track.album_name, str(track.actual_track_number), track.title))
+                            print ('{0} - {1} - {2} - {3}'.format(track.artist, track.album, str(track.track_number), track.title))
                     else:
                         #no file to play, should remove track object from master list
                         print('no file found, removing from library {0}'.format(str(track.file_object)))
@@ -236,7 +240,7 @@ elif args.command == 'tag':
     for field in args.field:
         if field == 'track':
             for filename, track in tracks_by_filename.items():
-                if track.actual_track_number is 0:
+                if track.track_number is 0:
                     #could not find track number, try to figure it out
                     regex = re.search('^\d+', os.path.basename(str(track.file_object)))
                     if regex is not None:
@@ -249,7 +253,7 @@ elif args.command == 'tag':
                             library_changed = True
         elif field == 'album':
             for filename, track in tracks_by_filename.items():
-                if track.album_name is None or track.album_name == '':
+                if track.album is None or track.album == '':
                     print('file {0} has no album name'.format(filename))
         elif field == 'artist':
             for filename, track in tracks_by_filename.items():

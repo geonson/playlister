5e03dcfdd380545e38d7d378f86fe3910e30f6e4
Kevin Johnson <geonson@gmail.com>
2020-06-24 10:32:46 -0700

    linux compile update

--- build_bin.bash
+++ build_bin.bash
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]
 then
   #Linux compile
   mkdir -p bin
-  gcc -Os -I /usr/include/python3.7m -L /usr/lib -o bin/playlister playlister.c -lpython3.7m -lpthread -lm -lutil -ldl
+  gcc -Os -I /usr/include/python3.8 -L /usr/lib -o bin/playlister playlister.c -lpython3.8 -lpthread -lm -lutil -ldl
 elif [[ "$OSTYPE" == "darwin"* ]]
 then
   #Mac OS compile

56fa68e809f4e63f7f1347c4cf984b583d70d88c
Kevin Johnson <geonson@gmail.com>
2023-06-28 17:30:51 -0700

    updating python version numbers

--- build_bin.bash
+++ build_bin.bash
@@ -6,9 +6,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]
 then
   #Linux compile
   mkdir -p bin
-  gcc -Os -I /usr/include/python3.8 -L /usr/lib -o bin/playlister playlister.c -lpython3.8 -lpthread -lm -lutil -ldl
+  gcc -Os -I /usr/include/python3.10 -L /usr/lib -o bin/playlister playlister.c -lpython3.10 -lpthread -lm -lutil -ldl
 elif [[ "$OSTYPE" == "darwin"* ]]
 then
   #Mac OS compile
-  gcc -Os -I ~/.pyenv/versions/3.8.3/include/python3.8 -L ~/.pyenv/versions/3.8.3/lib -o bin/playlister playlister.c -lpython3.8 -lpthread -lm -lutil -ldl
+  gcc -Os -I ~/.pyenv/versions/3.8.3/include/python3.8 -L ~/.pyenv/versions/3.8.3/lib -o bin/playlister playlister.c -lpython3.9 -lpthread -lm -lutil -ldl
 fi
